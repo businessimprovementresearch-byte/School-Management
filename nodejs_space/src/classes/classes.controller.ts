bed2632dd5d1a8cb4ea80f7d91ba8f08cb97a31b
businessimprovementresearch-byte <businessimprovementresearch@gmail.com>
2026-08-11 10:31:03 +0700

    Update classes.controller.ts

--- nodejs_space/src/classes/classes.controller.ts
+++ nodejs_space/src/classes/classes.controller.ts
@@ -82,12 +82,6 @@ export class ClassesController {
     return this.classesService.create(dto);
   }
 
-  @Post()
-  @Roles('ADMIN')
-  async create(@Body() dto: CreateClassDto): Promise<ClassDetailResponseDto> {
-    return this.classesService.create(dto);
-  }
-
   @Delete(':classId')
   @Roles('ADMIN')
   async remove(@Param('classId') classId: string): Promise<SuccessResponseDto> {
